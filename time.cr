f5df022438bff5ab035330e407f04cabfc1a6a8a
cameron <34195086+vulture001@users.noreply.github.com>
2019-09-28 18:15:29 -0400

    Create time.cr

---
+++ time.cr
@@ -0,0 +1,41 @@
+#Core
+kirmg = print
+qmkfg = input
+a = "z"
+b = "y"
+c = "x"
+d = "w"
+e = "v"
+f = "u"
+g = "t"
+h = "s"
+i = "r"
+j = "q"
+k = "p"
+l = "o"
+m = "n"
+n = "m"
+o = "l"
+p = "k"
+q = "j"
+r = "i"
+s = "h"
+t = "g"
+u = "f"
+v = "e"
+w = "d"
+x = "c"
+y = "b"
+z = "a"
+xln = ","
+kvi = "."
+yoz = " "
+vcx = "!"
+xlo = ":"
+
+name = qmkfg(m + z + n + v + xlo + yoz)
+kirmg(name)
+time = qmkfg(g + r + n + v + xlo + yoz)
+kirmg(time)
+
+kirmg(b + l + f + i + yoz + m + z + n + v + yoz + r + h + yoz + name + yoz + z + m + w + yoz + r + g + h + yoz + time + yoz + g + r + n + v + kvi)
